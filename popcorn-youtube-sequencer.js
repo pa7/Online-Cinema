0bc03b93a75a4cb557e2aa8b5913e9f851db2f17
Patrick Wied <patrick.wied@live.at>
2012-06-02 15:37:51 +0200

    added some docs + some style changes

--- popcorn-youtube-sequencer.js
+++ popcorn-youtube-sequencer.js
@@ -1,30 +1,34 @@
+/*
+ * PopcornYoutubeSequencer
+ *
+ * Public Interface
+ * - add: sets an array of youtube urls
+ * - play: starts a loop of set trailers
+ * - destroy: destroys the player used for the trailer-loop
+ */
 var PopcornYoutubeSequencer = (function(){
-  var player = null;
-  var urls = [];
-  var currentTrailer = 0;
-
-  var add = function(youtubeUrls){
-    urls = youtubeUrls;
-  };
-
-  var play = function(){
-    player = Popcorn.youtube('#trailers', urls[currentTrailer]);
-    player.on('ended', next );
-    player.play();
-  };
-
-  var next = function(){
-    currentTrailer++;
-    player.destroy();
-    if(currentTrailer >= urls.length){
-      currentTrailer = 0;
-    }
-    play();
-  };
-
-  var destroy = function(){
-    player.destroy();
-  };
+  var player = null,
+      urls = [],
+      currentTrailer = 0,
+      add = function(youtubeUrls){
+        urls = youtubeUrls;
+      },
+      play = function(){
+        player = Popcorn.youtube('#trailers', urls[currentTrailer]);
+        player.on('ended', next );
+        player.play();
+      },
+      next = function(){
+        currentTrailer++;
+        player.destroy();
+        if(currentTrailer >= urls.length){
+          currentTrailer = 0;
+        }
+        play();
+      },
+      destroy = function(){
+        player.destroy();
+      };
 
   return {
     add: add,
