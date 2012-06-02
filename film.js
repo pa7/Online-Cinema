85f0f613785a8911a962db66e0e4faad7a8dd59b
Jan Monschke <jan.monschke@gmail.com>
2012-06-02 16:44:14 +0200

    add film object

--- film.js
+++ film.js
@@ -1,4 +1,8 @@
 $(function(){
+  var film = {
+    startsAt : 333,
+    url: "films/big_buck_bunny_480p_surround-fix.avi"
+  }
   var curtainAnimationDuration = 200;
   PopcornYoutubeSequencer.add(["http://www.youtube.com/watch?v=cd-go0oBF4Y&controls=0", "http://www.youtube.com/watch?v=Zm7r491n-8o&controls=0"]);
   var startPlaying = function(){
